3ac4d864a2c59ce23c1b4f50782218eea96c02c8
Israel Díaz <progbass@gmail.com>
2023-12-21 19:40:29 -0600

    Installed Algolia InstantSearch and added autocomplete component

--- template_src/src/assets/componentes/genericos/busquedaBlack.vue
+++ template_src/src/assets/componentes/genericos/busquedaBlack.vue
@@ -1,143 +1,166 @@
 <template>
-    <div class="row w-100 m-0 p-1px justify-content-center border-gray0-2 border-radius-30px ">
-        <div class="w-10vw text-right">
+    <div class="row w-100 m-0 p-1px justify-content-center border-gray5-2 border-radius-30px ">
+        <!-- <div class="w-10vw text-right">
             <i class="material-icons letra-gray0-25 my-auto pt-5px">search</i>
-        </div>
-        <div class="col my-auto p-0">
-    
+        </div> -->
+        <div class="col my-auto w-100 p-0">
             <div class="row w-100 m-0">
                 <!-- <div class="w-auto position-absolute top-0px right-5px z-200" @click="$emit('input', null)">
                     <icono icono="clear" :clase=" `letra-gray3-26` " />
                 </div> -->
-                <input  :id="id" type="text" class="busqueda-input w-100 p-0 pl-1 pr-4 border-radius-28px color-blanco" 
+                <Autocomplete 
+                    :key="'desktop-search'" 
+                    @input="val => {$emit('input', val);}" 
+                />
+                <!-- <ais-search-box /> -->
+                <!-- <input  :id="id" type="text" class="busqueda-input w-100 p-0 pl-1 pr-4 border-radius-28px color-blanco" 
                         :placeholder="holder || 'Buscar ...'" 
                         :value="value" 
                         @input="$emit('input',$event.target.value); initX();" 
                         @change="$emit('change', $event.target.value); "
-                        @keyup.enter="enter" >
+                        @keyup.enter="enter" > -->
             </div>
-            
         </div>
-    
-        <div v-if="close && value" class="col-auto text-right px-0 pr-2" @click="$emit('input', null)" >
+
+        <!-- <div v-if="close && value" class="col-auto text-right px-0 pr-2" @click="$emit('input', null)">
             <i class="material-icons letra-gray0-25 my-auto pt-5px">close</i>
-        </div>
+        </div> -->
     </div>
-    </template>
-    <script>
+</template>
+    
+<script>
+import Autocomplete from './Autocomplete.vue';
+const moment = require('moment');
 
-    const moment = require('moment');
+export default {
+    components: {
+        Autocomplete,
+    },
+    props: [
+        'value',
+        'holder',
+        'close',
+        'date',
+        'type',
+        'loader',
+        'input',
+        'password',
+        'clase',
+        'noNumeros',
+        'placeholder',
+        'maxlength',
+        'disabled',
+        'auto',
+        'textarea',
+        'rows',
+        'texto',
+        'scroll',
+        'offset',
+        'fixed',
+        'contendorId',
+        'textcolor'
+    ],
+    data() {
+        return {
+            id: _.uniqueId('input_'),
+            elemento: null,
+            hora: null,
+        };
+    },
+    computed: {
+        android() {
+            // console.log("ANDORIDNG", this.$store.getters.deviceready);
+            if (this.$store.getters.deviceready) {
+                return typeof device != 'undefined' && device.platform == 'Android'
+            }
+            return false;
+        },
+    },
+    mounted() {
+        setTimeout(
+            () => {
+                this.initElemento();
+            }, 350);
+    },
+    methods: {
+        initElemento() {
+            if (document.getElementById(this.id)) {
+                this.elemento = document.getElementById(this.id);
+                if (this.elemento) {
+                    this.focus();
+                }
+            }
+            else {
+                setTimeout(this.initElemento, 300);
+            }
+        },
 
-    export default {
-        props:  [
-            'value',
-            'holder',
-            'close',
-            'date',
-            'type',
-            'loader',
-            'input',
-            'password',
-            'clase',
-            'noNumeros',
-            'placeholder',
-            'maxlength',
-            'disabled',
-            'auto', 
-            'textarea', 
-            'rows', 
-            'texto' ,
-            'scroll',
-            'offset',
-            'fixed',
-            'contendorId' ,
-            'textcolor'
-        ],
-        data(){
-            return {
-                id: _.uniqueId('input_'),
-                elemento: null,
-                hora: null,
-            };
-        },
-        computed:{
-            android(){
-                // console.log("ANDORIDNG", this.$store.getters.deviceready);
-                if(this.$store.getters.deviceready){
-                    return typeof device != 'undefined' && device.platform == 'Android'
-                }
-                return false;
-            },
-        },
-        mounted() {
-            setTimeout(
-                ()=>{
-                    this.initElemento();
-            },350);
-        },
-        methods:{
-            initElemento(){
-                if(document.getElementById(this.id)){
-                    this.elemento = document.getElementById(this.id);
-                    if(this.elemento){
-                        this.focus();
-                    }
-                }
-                else{
-                    setTimeout(this.initElemento, 300);
-                }
-            },
-
-            initX(){
-                console.log("inti 123");
-                if(!this.hora){
-                    this.hora = moment().format('YYYY-MM-DD HH:mm:ss');
-                }else{
-                    let n = moment().format('YYYY-MM-DD HH:mm:ss');
-                    setTimeout(()=>{
-                        console.log("SE EJUCUTA TIME", this.hora, n, n == this.hora)
-                        if(this.hora){
-                            if(this.hora == n){
-                                if(this.value){
-                                    if(this.loader){
-                                        this.$store.commit('initLoader');
-                                        setTimeout(()=>{
-                                            this.$store.commit('finishLoader');
-                                        },1000*1.5);
-                                    }
+        initX() {
+            console.log("inti 123");
+            if (!this.hora) {
+                this.hora = moment().format('YYYY-MM-DD HH:mm:ss');
+            } else {
+                let n = moment().format('YYYY-MM-DD HH:mm:ss');
+                setTimeout(() => {
+                    console.log("SE EJUCUTA TIME", this.hora, n, n == this.hora)
+                    if (this.hora) {
+                        if (this.hora == n) {
+                            if (this.value) {
+                                if (this.loader) {
+                                    this.$store.commit('initLoader');
+                                    setTimeout(() => {
+                                        this.$store.commit('finishLoader');
+                                    }, 1000 * 1.5);
                                 }
                             }
                         }
-                    }, 1000*0.8);
-                    this.hora = moment().format('YYYY-MM-DD HH:mm:ss');
-                }
-            },
-    
-            focus(){
-                if(!this.fixed){
-                    if(this.android || this.scroll){
-                        $(`#${this.id}`).focus(()=>{
-                            setTimeout(()=>{
-                                const offset = this.offset || 450;
-                                // console.log("SETING OFFSET", offset);
-                                if(this.contendorId){
-                                    $(`#${this.contendorId}`).animate({ scrollTop: offset }, 330);
-                                }
-                                else{
-                                    $('.contenedor-page-tabs').animate({ scrollTop: offset }, 330);
-                                }
-                            },350);
-                        });
                     }
-                }
-            },
-    
-            enter(){
-                if(this.elemento){
-                    this.elemento.blur();
-                }
-                this.$emit('enter');
+                }, 1000 * 0.8);
+                this.hora = moment().format('YYYY-MM-DD HH:mm:ss');
             }
         },
-    }
-    </script>
+
+        focus() {
+            if (!this.fixed) {
+                if (this.android || this.scroll) {
+                    $(`#${this.id}`).focus(() => {
+                        setTimeout(() => {
+                            const offset = this.offset || 450;
+                            // console.log("SETING OFFSET", offset);
+                            if (this.contendorId) {
+                                $(`#${this.contendorId}`).animate({ scrollTop: offset }, 330);
+                            }
+                            else {
+                                $('.contenedor-page-tabs').animate({ scrollTop: offset }, 330);
+                            }
+                        }, 350);
+                    });
+                }
+            }
+        },
+
+        enter() {
+            if (this.elemento) {
+                this.elemento.blur();
+            }
+            this.$emit('enter');
+        }
+    },
+}
+</script>
+
+<style>
+.aa-Autocomplete .aa-Form{
+    width: 100%;
+    background: transparent;
+    border: none;
+}
+.aa-Label{
+    width: 47px;
+    height: auto;
+    margin-bottom: 0px;
+}
+.aa-Label svg{
+    color: #989797;
+}
+</style>
--- template_src/src/main.vue
+++ template_src/src/main.vue
@@ -1,31 +1,34 @@
 <template>
-
     <!-- App -->
-    <f7-app :params="f7params">
-        <f7-panel left cover>
-            <f7-view class="left-view" url="/menu" :main="true" name="menu" id="menu" :iosSwipeBack="false" />
-        </f7-panel>
+    <ais-instant-search :search-client="searchClient" index-name="products">
+        <f7-app :params="f7params">
+            <f7-panel left cover>
+                <f7-view class="left-view" url="/menu" :main="true" name="menu" id="menu" :iosSwipeBack="false" />
+            </f7-panel>
 
-        <f7-panel right cover>
-            <f7-view class="back-color-rojo" url="/menu" tabActive :main="true" :iosSwipeBack="false" name="por_ordenar" id="por_ordenar"></f7-view>
-         </f7-panel>
+            <f7-panel right cover>
+                <f7-view class="back-color-rojo" url="/menu" tabActive :main="true" :iosSwipeBack="false" name="por_ordenar" id="por_ordenar"></f7-view>
+            </f7-panel>
 
-        <f7-view url="/views" :main="true" :iosSwipeBack="false" class="ios-edges"></f7-view>
+            
+                <f7-view url="/views" :main="true" :iosSwipeBack="false" class="ios-edges"></f7-view>
+            
 
-        <!-- <modalCalificar /> -->
-        <menuModal />
-        <modalNotificacion />
-        <globalMsn />
-        <modalSession />
-        <selectImagen />
-        <photoswipe />
-        <modal-loader />
-    </f7-app>
-
+            <!-- <modalCalificar /> -->
+            <menuModal />
+            <modalNotificacion />
+            <globalMsn />
+            <modalSession />
+            <selectImagen />
+            <photoswipe />
+            <modal-loader />
+        </f7-app>
+    </ais-instant-search>
 </template>
-<script>
 
-// Import Routes...
+<script>
+import algoliasearch from 'algoliasearch/lite';
+import 'instantsearch.css/themes/algolia-min.css';
 import { 
     f7App, 
     f7Panel, 
@@ -33,13 +36,23 @@ import {
     f7Statusbar 
 } from 'framework7-vue';
 
+
+// Import Routes
 import routes from './routes.js';
 
+//
 let theme = 'auto';
 if (document.location.search.indexOf('theme=') >= 0) {
   theme = document.location.search.split('theme=')[1].split('&')[0];
 }
 
+// Algolia configuration
+const searchClient = algoliasearch(
+    "YQ73Q1UV03",
+    "b0ebda9bda7d1ddf47211de6759dd800"
+);
+
+//
 export default {
     components: {
       f7App,
@@ -49,6 +62,7 @@ export default {
     },
     data() {
         return {
+            searchClient,
             f7params: {
                 theme,
                 routes,
